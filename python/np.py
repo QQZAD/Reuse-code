7f6604468074cb34ad05b0863bbc5f33d96846cb
QQZAD <2298070398@qq.com>
2020-11-13 19:11:56 +0800

    reuse-code

---
+++ python/np.py
@@ -0,0 +1,16 @@
+# Polynomial Problem
+# P问题指的是在多项式时间计算机可以求解的问题
+
+# Nondeterministic Polynomial Problem
+# NP问题指的是在多项式时间可以验证给定解正确与否的问题
+# 所有P问题都是NP问题
+# NP问题的本质是P=NP？证明或者证伪
+
+# Nondeterministic Polynomial Complete Problem
+# NPC问题指的是NP问题中最难解决的问题
+# 只要存在一个在多项式时间内可解的NPC问题，那么所有NP问题都可以求解
+# 正是NPC问题的存在导致P=NP难以置信
+
+# NP-hard Problem
+# NP难问题指的是这样的问题
+# 只要存在一个在多项式时间内可解的问题，那么所有NP问题都可以求解
