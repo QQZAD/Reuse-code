48116eaac78d7bcb8e11e887f38d457ada9aad06
QQZAD <2298070398@qq.com>
2021-03-26 09:46:33 +0800

    reuse-code

--- cpp/link.cpp
+++ cpp/link.cpp
@@ -71,6 +71,42 @@ void print(node *head)
     printf("\n");
 }
 
+void createCycle(node *head)
+{
+    int i = 0;
+    node *p, *q = NULL;
+    for (p = head; p->next != NULL; p = p->next)
+    {
+        i++;
+        if (i == 4)
+        {
+            q = p;
+        }
+    }
+    p->next = q;
+}
+
+void detectCycle(node *head)
+{
+    node *slow = head, *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            fast = head;
+            while (fast != slow)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            printf("cycle-%d\n", fast->val);
+            break;
+        }
+    }
+}
+
 int main()
 {
     node *head;
@@ -78,7 +114,9 @@ int main()
     // print(head);
     // reverse(head);
     // print(head);
-    lastkNode(head, 6);
+    // lastkNode(head, 6);
+    createCycle(head);
+    detectCycle(head);
 }
 /*
 cd cpp;g++ -g link.cpp -o link;./link;cd ..
